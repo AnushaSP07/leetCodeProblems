64b701222309b35124e15f80bab6cc95ceec86df
Anusha SP <anushaspga07@gmail.com>
2022-01-24 09:06:15 +0530

    Create DetectCapital_520.js
    Daily Challenge JS

---
+++ LeetCode-DailyChallenges/DetectCapital_520.js
@@ -0,0 +1,37 @@
+/*
+
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+
+ 
+
+Example 1:
+
+Input: word = "USA"
+Output: true
+Example 2:
+
+Input: word = "FlaG"
+Output: false
+
+*/
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+    if (word.toUpperCase() === word) {
+        return true;
+    }
+
+    let subword = word.substring(1)
+    if (subword.toLowerCase() === subword) {
+        return true
+    }
+    return false
+};
