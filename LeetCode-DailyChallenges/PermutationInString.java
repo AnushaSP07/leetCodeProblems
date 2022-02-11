796b8b90ec70caba5d256c3d201f229598278792
Anusha SP <anushaspga07@gmail.com>
2022-02-11 09:28:48 +0530

    Create PermutationInString.java
    Daily challenge java

---
+++ LeetCode-DailyChallenges/PermutationInString.java
@@ -0,0 +1,56 @@
+/*
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+ 
+
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+*/
+
+
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        
+          //if s1 itself is larger return false
+        if(s1.length()>s2.length()) {
+            return false;
+        }
+        //store frequency of s1 and s2. for s1 character add 1 and for s1 subtract 1
+        int[] freq = new int[26];
+        // prepare freq map till s1 length
+        for (int i = 0; i < s1.length(); i++) {
+			freq[s1.charAt(i) - 'a'] += 1;
+			freq[s2.charAt(i) - 'a'] -= 1;
+		}
+        //loop till length exhaust. at a time we will compare total s1.length characters.
+        for(int i=0;i<s2.length()-s1.length();i++) {
+                //check if s1 and s2 are anagram of each other.
+                if(checkAnagram(freq)) {
+                   return true; 
+                }
+                //if not update freq map to remove s2 first character and add s2 next character 
+                freq[s2.charAt(i) - 'a'] += 1;
+			    freq[s2.charAt(i + s1.length()) - 'a'] -= 1;
+        }
+        //check for last comparison
+        return checkAnagram(freq);
+    }
+    //check if freq map has all zero. it will be true for anagram
+    private boolean checkAnagram(int[] freq) {
+        for(int i=0;i<freq.length;i++) {
+            if(freq[i]!=0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
