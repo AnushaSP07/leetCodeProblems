d1f43648b00c58db374d3f8912f8df88e9d0e286
Anusha SP <anushaspga07@gmail.com>
2022-05-24 09:13:10 +0530

    Create ArrangingCoins.java
    Binary search study plan question 11

---
+++ BinarySearchStudyPlan/ArrangingCoins.java
@@ -0,0 +1,53 @@
+/*
+
+441. Arranging Coins
+Easy
+
+2251
+
+1029
+
+Add to List
+
+Share
+You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+Given the integer n, return the number of complete rows of the staircase you will build.
+
+ 
+
+Example 1:
+
+
+Input: n = 5
+Output: 2
+Explanation: Because the 3rd row is incomplete, we return 2.
+Example 2:
+
+
+Input: n = 8
+Output: 3
+Explanation: Because the 4th row is incomplete, we return 3.
+
+*/
+
+class Solution {
+    public int arrangeCoins(int n) {
+        long start = 0, end = n;
+        long mid , current;
+        
+        while(start <= end){
+            mid = start + (end - start)/2;
+            current = mid * (mid + 1)/2;
+            
+            if(current == n)
+                return (int)mid;
+            if(n < current){
+                end = mid - 1;
+            } else{
+                start = mid + 1;
+            }  
+        }
+        return (int)end;
+    }
+}
