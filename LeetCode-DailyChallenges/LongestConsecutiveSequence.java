0246d4ba6f9103d1cf5143d57bb2273a45878f61
Anusha SP <anushaspga07@gmail.com>
2022-07-05 10:27:49 +0530

    Create LongestConsecutiveSequence.java
    July 5th 2022

---
+++ LeetCode-DailyChallenges/LongestConsecutiveSequence.java
@@ -0,0 +1,58 @@
+/*
+
+128. Longest Consecutive Sequence
+Medium
+
+10671
+
+465
+
+Add to List
+
+Share
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+
+*/
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+       int result = 0; // answer len
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums) set.add(i); 
+    // add all elements in a set, we dont require duplicates because - 
+    //suppose we have 1 1 2 2 3 3 in the array the max len will be 3 - (1 2 3) doesnt matter how many times a number is present 
+        for(int i:nums){
+    // the idea is to assume the present value "i" the center of the sequence in which it is present 
+    // then we will go left and right of it to find the length of its sequence
+    // suppose we have 5 1 2 3 4 6 7 8, now the first element we have is 5
+         int max = 1,prevVal = i-1,nextVal = i+1; 
+    // max is the current length as we have one elemet in the sequence that is 5
+    // now for 5 prevVal = 4
+         while(set.contains(prevVal)){ // if 4 is present in the set i.e. in the array
+               max++; // increase the sequence len
+               set.remove(prevVal--); // remove 4 ans decremennt prevVal to 3 and continue 
+           }
+    // removing the elements because an element can only be part of only one consecutive sequence
+            while(set.contains(nextVal)){ // same for the nextVal of the sequence
+                max++;
+                set.remove(nextVal++);
+            }
+            result = Math.max(result,max); // maintaining max len 
+        }
+        return result;
+    }
+}
