43c3a42374619436ff392abb9c597d0ab6860bb5
Anusha SP <anushaspga07@gmail.com>
2022-08-26 23:14:12 +0530

    Create ReorderedPowerOf_2.java

---
+++ LeetCode-DailyChallenges/ReorderedPowerOf_2.java
@@ -0,0 +1,49 @@
+/*
+
+869. Reordered Power of 2
+Medium
+
+1656
+
+340
+
+Add to List
+
+Share
+You are given an integer n. We reorder the digits in any order (including the original order) such that the leading digit is not zero.
+
+Return true if and only if we can do this so that the resulting number is a power of two.
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: true
+Example 2:
+
+Input: n = 10
+Output: false
+
+*/
+
+class Solution {
+    public boolean reorderedPowerOf2(int N) {
+        int[] A = count(N);
+        for (int i = 0; i < 31; ++i)
+            if (Arrays.equals(A, count(1 << i)))
+                return true;
+        return false;
+    }
+
+    // Returns the count of digits of N
+    // Eg. N = 112223334, returns [0,2,3,3,1,0,0,0,0,0]
+    public int[] count(int N) {
+        int[] ans = new int[10];
+        while (N > 0) {
+            ans[N % 10]++;
+            N /= 10;
+        }
+        return ans;
+    }
+}
